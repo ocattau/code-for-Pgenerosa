9e0ee07aec40f2e3fdcd75c4bf8c4159f3b0afa6
Olivia Cattau <81712104+ocattau@users.noreply.github.com>
2022-02-10 16:21:37 -0800

    Update slurm_p.generosa.sh

--- Pgenerosa/slurm_p.generosa.sh
+++ Pgenerosa/slurm_p.generosa.sh
@@ -7,22 +7,22 @@
 ## Nodes
 #SBATCH --nodes=1
 ## Walltime (days-hours:minutes:seconds format)
-#SBATCH --time=00-12:00:00
+#SBATCH --time=05-00:00:00
 ## Memory per node
 #SBATCH --mem=500G
 ##turn on e-mail notification
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ocattau@uw.edu
 ## Specify the working directory for this job
-#SBATCH --chdir=/gscratch/srlab/ocattau/p.generosa
+#SBATCH --chdir=/gscratch/srlab/ocattau/p.generosa #home directory
 
 
-/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin/blastx \
--query /gscratch/srlab/ocattau/p.generosa/Panopea-generosa-v1.0.fa \
--db /gscratch/srlab/ocattau/p.generosa/Pgenerosa_transcriptome_v5.fasta
--out /gscratch/srlab/ocattau/p.generosa/Panopea-generosa-uniprot_blastx.tab \
--evalue 1E-20 \
--num_threads 30 \
+/gscratch/srlab/programs/ncbi-blast-2.8.1+/bin/blastx \ #load ncbi blastx software
+-query /gscratch/srlab/ocattau/p.generosa/Pgenerosa_transcriptome_v5.fasta \ #this is the query file, should be the geoduck transcriptome
+-db /gscratch/srlab/blastdbs/ncbi-sp-v5_20210224/swissprot \ #blast databse, should always be this swissprot in order to get gene annotations
+-out /gscratch/srlab/ocattau/p.generosa/Panopea-generosa-uniprot_blastx.tab \ #output file 
+-evalue 1E-05 \
+-num_threads 30 \ #number of threads is going to be +28
 -max_target_seqs 1 \
 -max_hsps 1 \
 -outfmt 6
