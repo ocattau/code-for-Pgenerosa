cf78effee533201c5eec2c5b39e62207cd188d4d
Olivia Cattau <ocattau@uw.edu>
2023-05-09 14:21:13 -0700

    added new tables

--- blasting/050423_Pgen_solida_genome.sh
+++ blasting/050423_Pgen_solida_genome.sh
@@ -6,7 +6,7 @@
 ## Nodes
 #SBATCH --nodes=1
 ## Walltime (days-hours:minutes:seconds format)
-#SBATCH --time=03-00:00:00
+#SBATCH --time=00-01:00:00
 ## Memory per node
 #SBATCH --mem=500G
 ##turn on e-mail notification
